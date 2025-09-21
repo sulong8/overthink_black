0467da85af156b8d752d4cddf721c0c8aaf65ecf
caowei <caowei0306@gmail.com>
2025-09-21 22:47:28 +0800

    init

--- model.py
+++ model.py
@@ -81,13 +81,7 @@ class AttackerModel:
         clean_text = re.sub(r"^```json|```$", "", response.strip(), flags=re.MULTILINE).strip()
         res = json.loads(clean_text)
         return res
-    
 
-
-if __name__ == "__main__":
-    attacker = AttackerModel("deepseek-chat")
-    q = "Kylar went to the store to buy glasses for his new apartment. One glass costs $5, but every second glass costs only 60% of the price. Kylar wants to buy 16 glasses. How much does he need to pay for them?"
-    print(attacker.split_question(q))
     
     
 
